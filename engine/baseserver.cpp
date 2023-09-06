d6ff36b85cd955815816d2dbe7a3ac91a32629dd
hasandramali <93338836+hasandramali@users.noreply.github.com>
2023-09-06 16:00:18 +0300

    Update baseserver.cpp

--- engine/baseserver.cpp
+++ engine/baseserver.cpp
@@ -723,7 +723,7 @@ bool CBaseServer::ProcessConnectionlessPacket(netpacket_t * packet)
 				// checking.
 				const char *pszVersionInP4 = "2000";
 				const char *pszVersionString = GetSteamInfIDVersionInfo().szVersionString;
-				if ( V_strcmp( pszVersionString, pszVersionInP4 ) && V_strcmp( productVersion, pszVersionInP4 ) )
+				/**if ( V_strcmp( pszVersionString, pszVersionInP4 ) && V_strcmp( productVersion, pszVersionInP4 ) )
 				{
 					int nVersionCheck = Q_strncmp( pszVersionString, productVersion, V_strlen( pszVersionString ) );
 					if ( nVersionCheck < 0 )
@@ -736,7 +736,7 @@ bool CBaseServer::ProcessConnectionlessPacket(netpacket_t * packet)
 						RejectConnection( packet->from, clientChallenge, "#GameUI_ServerRejectNewVersion" );
 						break;
 					}
-				}
+				}**/
 
 // 				if ( Steam3Server().BSecure() && bClientPlugins )
 // 				{
